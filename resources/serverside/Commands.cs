e955931cbbf3a3465f43a7fbc1afcc42d7469bd1
Nazar Solonovich <135126220+SOLN4R@users.noreply.github.com>
2024-05-16 10:50:46 +0300

    0.01
    - Fundamental settings
    - Clientside connection

---
+++ resources/serverside/Commands.cs
@@ -0,0 +1,22 @@
+﻿using AltV.Net;
+using AltV.Net.Data;
+using AltV.Net.Elements.Entities;
+using AltV.Net.Resources.Chat.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace serverside
+{
+    public class Commands : IScript
+    {
+        [Command("test")]
+        public static void Test(IPlayer iplayer, string Message)
+        {
+            Message = "Hello from Serverside!";
+            iplayer.Emit("SERVER_CLIENT_Message", Message);
+        }
+    }
+}
--- resources/serverside/Events.cs
+++ resources/serverside/Events.cs
@@ -12,14 +12,14 @@ namespace serverside
     public class Events : IScript
     {
         [ScriptEvent(ScriptEventType.PlayerConnect)]
-        public void PlayerConnect(IPlayer player, String reason)
+        public void PlayerConnect(IPlayer iplayer, String reason)
         {
-            player.Spawn(new AltV.Net.Data.Position(0, 0, 0), 0);
-            player.Model = (uint) PedModel.FreemodeMale01;
+            iplayer.Spawn(new AltV.Net.Data.Position(0, 0, 0), 0);
+            iplayer.Model = (uint) PedModel.FreemodeMale01;
         }
 
         [ScriptEvent(ScriptEventType.PlayerDisconnect)]
-        public void PlayerDisconnect(IPlayer player, String reason)
+        public void PlayerDisconnect(IPlayer iplayer, String reason)
         {
             
         }
